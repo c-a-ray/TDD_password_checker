78b9fa36df966d83a102cecc70e1210540a616de
c-a-ray <c.a.ray@outlook.com>
2022-02-14 14:09:08 -0800

    Add check for at least 1 digit in pwd in check_pwd

--- check_pwd.py
+++ check_pwd.py
@@ -11,12 +11,16 @@ Specifications:
 def check_pwd(pwd: str) -> bool:
     has_lowercase_letter = False
     has_uppercase_letter = False
+    has_digit = False
     for c in pwd:
         if c.islower():
             has_lowercase_letter = True
         elif c.isupper():
             has_uppercase_letter = True
+        elif c.isdigit():
+            has_digit = True
 
     pwd_len = len(pwd)
     return pwd_len >= 8 and pwd_len <= 20 \
-        and has_lowercase_letter and has_uppercase_letter
+        and has_lowercase_letter and has_uppercase_letter \
+            and has_digit
