40c6abe4bda6e42f9771be5d8134ade204fac64b
c-a-ray <c.a.ray@outlook.com>
2022-02-14 14:04:44 -0800

    Add check for at least 1 uppercase letter in check_pwd

--- check_pwd.py
+++ check_pwd.py
@@ -7,11 +7,16 @@ Specifications:
     * Must contain at least one symbol from: ~`!@#$%^&*()_+-=
 """
 
+
 def check_pwd(pwd: str) -> bool:
     has_lowercase_letter = False
+    has_uppercase_letter = False
     for c in pwd:
         if c.islower():
             has_lowercase_letter = True
-    
+        elif c.isupper():
+            has_uppercase_letter = True
+
     pwd_len = len(pwd)
-    return pwd_len >= 8 and pwd_len <= 20 and has_lowercase_letter
+    return pwd_len >= 8 and pwd_len <= 20 \
+        and has_lowercase_letter and has_uppercase_letter
