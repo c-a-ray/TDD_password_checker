9e8963dba8ee012b966fa9221217d71236a4a78b
c-a-ray <c.a.ray@outlook.com>
2022-02-14 13:16:53 -0800

    Add check_pwd.py and tests.py

---
+++ check_pwd.py
@@ -0,0 +1,11 @@
+"""
+Specifications:
+    * Must be between 8 and 20 characters (inclusive)
+    * Must contain at least one lowercase letter
+    * Must contain at least one uppercase letter
+    * Must contain at least one digit
+    * Must contain at least one symbol from: ~`!@#$%^&*()_+-=
+"""
+
+def check_pwd(pwd: str) -> bool:
+    pass
