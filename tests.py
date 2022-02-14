a85a9ac96b0de7dd851b1461a8d1dd858b195fb1
c-a-ray <c.a.ray@outlook.com>
2022-02-14 13:38:18 -0800

    Add test_empty_str to test that empty string is invalid

--- tests.py
+++ tests.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
     def try_pwd(self, pwd: str, is_valid: bool):
         self.assertEqual(check_pwd(pwd), is_valid)
 
+    def test_empty_str(self):
+        self.try_pwd('', False)
+
 
 if __name__ == '__main__':
     unittest.main()
