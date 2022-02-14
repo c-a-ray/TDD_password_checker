26bc8a11ee098251fd751eb10f0168af5c9a4669
c-a-ray <c.a.ray@outlook.com>
2022-02-14 13:31:44 -0800

    Add try_pwd helper function

--- tests.py
+++ tests.py
@@ -3,8 +3,9 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
-  pass
+    def try_pwd(self, pwd: str, is_valid: bool):
+        self.assertEqual(check_pwd(pwd), is_valid)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
