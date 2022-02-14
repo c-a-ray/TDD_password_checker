b08ef97417835e413231670191ad6ef54320efd9
c-a-ray <c.a.ray@outlook.com>
2022-02-14 14:12:16 -0800

    Add check for at least 1 valid symbol in pwd in check_pwd

--- check_pwd.py
+++ check_pwd.py
@@ -7,11 +7,13 @@ Specifications:
     * Must contain at least one symbol from: ~`!@#$%^&*()_+-=
 """
 
+VALID_SYMBOLS = "~`!@#$%^&*()_+-="
 
 def check_pwd(pwd: str) -> bool:
     has_lowercase_letter = False
     has_uppercase_letter = False
     has_digit = False
+    has_symbol = False
     for c in pwd:
         if c.islower():
             has_lowercase_letter = True
@@ -19,8 +21,10 @@ def check_pwd(pwd: str) -> bool:
             has_uppercase_letter = True
         elif c.isdigit():
             has_digit = True
+        elif c in VALID_SYMBOLS:
+            has_symbol = True
 
     pwd_len = len(pwd)
     return pwd_len >= 8 and pwd_len <= 20 \
         and has_lowercase_letter and has_uppercase_letter \
-            and has_digit
+            and has_digit and has_symbol
